63ce132f74c6ad0adcd242a2261e6a8bdcf84a34
Saul <saul.rodriguez.naranjo@gmail.com>
2023-02-24 20:17:18 +0100

    Comenzamos a implementar el ejemplo con OCL.js

---
+++ Actividades Academicamente Dirigidas/OCL_JS_PROYECTO/Empleado.js
@@ -0,0 +1,14 @@
+class Empleado {
+    #idEmpleado;
+    #dni;
+    #codigoDeEmpleado;
+    #nombre;
+    #apellidos;
+    #fechaDeNacimiento;
+
+    get idEmpleado() {
+        return this.idEmpleado;
+    }
+
+    
+}
